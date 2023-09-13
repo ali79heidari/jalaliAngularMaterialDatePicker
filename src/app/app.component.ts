36fd7d3c8f1e41214d456d6ed5da854d855d07ce
a.heidari <User@DIDAR>
2023-09-13 11:12:55 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
+import * as jalaliMoment from 'jalali-moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/** https://github.com/fingerpich/jalali-moment#angular */
 export class AppComponent {
-  title = 'jalaliAngularMaterialDatePicker';
+
+  iranHoliday: Record<number, number[]> = {
+    1: [1, 2, 3, 4, 12, 13],
+    2: [],
+    3: [14, 15],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [22],
+    12: [29]
+  };
+
+  islamicHoliday: Record<number, number[]> = {
+    1: [9, 10],
+    2: [20, 28, 30],
+    3: [8, 17],
+    4: [],
+    5: [],
+    6: [3],
+    7: [13, 27],
+    8: [15],
+    9: [21],
+    10: [1, 2, 25],
+    11: [],
+    12: [10, 18]
+  };
+
+  dateClass: MatCalendarCellClassFunction<jalaliMoment.Moment> = (cellDate, view) => {
+    if (view === 'month') {
+      const jMonth = cellDate.jMonth() + 1;
+      const jDate = cellDate.jDate();
+      const jDay = cellDate.jDay() + 1;
+      let iMonth: any, iDate: any;
+      this.hijriDate(cellDate.toDate(), (year, month, day) => {
+        iMonth = month;
+        iDate = day;
+      });
+      if (this.iranHoliday[jMonth]?.includes(jDate) || jDay === 7 || this.islamicHoliday[+iMonth]?.includes(+iDate)) {
+        return 'example-custom-date-class';
+      }
+    }
+    return '';
+  };
+
+
+  hijriDate(date: Date, hijriDate: (year: string, month: string, day: string) => void): any {
+    const myFormat: string = 'fa-u-ca-islamic-umalqura-nu-latn';
+    const myDate: Date = new Date(date);
+    const year: string = new Intl.DateTimeFormat(myFormat, {year: 'numeric'}).format(myDate).split(" ")[0];
+    const month: string = new Intl.DateTimeFormat(myFormat, {month: 'numeric'}).format(myDate);
+    const day: string = new Intl.DateTimeFormat(myFormat, {day: 'numeric'}).format(myDate);
+    return hijriDate(year, month, day);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatInputModule} from "@angular/material/input";
+import {JalaliMomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from "./jalalidatepicker";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {JALALI_MOMENT_FORMATS, MOMENT_FORMATS} from "./jalaliformat";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: JalaliMomentDateAdapter,
+      deps: [MAT_DATE_LOCALE]
+    },
+    {provide: MAT_DATE_LOCALE, useValue: 'fa'},
+    {
+      provide: MAT_DATE_FORMATS,
+      useFactory: (locale: string) => {
+        if (locale === 'fa') {
+          return JALALI_MOMENT_FORMATS;
+        } else {
+          return MOMENT_FORMATS;
+        }
+      },
+      deps: [MAT_DATE_LOCALE]
+    },
+    {provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: true}}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
